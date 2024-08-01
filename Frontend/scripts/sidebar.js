dd7794c79898b5237ca82389f1da8d4de494bdd8
adeola-praise <68433446+adeola-praise@users.noreply.github.com>
2024-08-01 10:43:21 -0900

    Reverse Changes

--- Frontend/scripts/sidebar.js
+++ Frontend/scripts/sidebar.js
@@ -122,13 +122,13 @@ document.addEventListener("DOMContentLoaded", function () {
         // Populate the template with lab result data
         labCard.querySelector(".lab-info").textContent = appointment?.notes;
         labCard.querySelector(".lab-test").textContent = appointment?.test_name;
-        const dateString = appointment?.test_date;
-        const date = new Date(dateString);
-        const options = { day: "numeric", month: "long", year: "numeric" };
-        const formattedDate = new Intl.DateTimeFormat("en-US", options).format(
-          date
-        );
-        labCard.querySelector(".lab-date").textContent = formattedDate;
+        labCard.querySelector(".lab-date").textContent = new Date(
+          appointment?.test_date
+        ).toLocaleDateString("en-US", {
+          day: "numeric",
+          month: "long",
+          year: "numeric",
+        });
 
         // Append the populated template to the container
         dashboardLabActivity.appendChild(labCard);
